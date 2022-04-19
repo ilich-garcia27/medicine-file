70aab001030c51babf4ac30722b91880c86e3cef
Ilich García <ilichgarcia27@gmail.com>
2022-04-19 08:53:15 -0600

    Creating HTML, SCSS, and medicine model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MedicineComponent } from './components/medicine/medicine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MedicineComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/models/medicine.model.ts
@@ -0,0 +1,9 @@
+export interface Medicine {
+  name: string,
+  quantity: number,
+  display: number,
+  concentration: number,
+  via: string,
+  frequency: number,
+  duration: string
+}
